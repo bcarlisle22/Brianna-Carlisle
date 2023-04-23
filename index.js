52e63cc13d8cb7b2be7a3610fb77a2f26c4f882a
Brianna Carlisle <103350106+bcarlisle22@users.noreply.github.com>
2023-04-23 16:30:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,112 @@
-const ProjectInfo = document.querySelector('linksandinfo');
 
-let showOrHide = function(){
-    ProjectInfo.getElementsByClassName.display = 'block';
+// show or hide on button click 01
+
+const divContainer = document.querySelector ('#linksandinfo');
+let isClicked = true;
+
+
+function showHide(){
+    if(isClicked){
+        divContainer.style.display = 'block';
+        isClicked = false;
+    }else{
+        divContainer.style.display ='none';
+        isClicked = true;
 }
+}
+
+
+// // rotate on click button  
+
+// let divSign = document.getElementById ('#plussign');
+
+// function rotatePlus(){
+//     if(isClicking){
+//         divSign.style.transform = 'rotate(90deg)';
+//         isClicking = false;
+//     }else{
+//         divSign.style.transform = 'rotate(-90deg)';
+//         isClicking = true;
+//     }
+// }
+
+// show or hide button 02
+
+const clickPlus = document.querySelector ('#linksandinfotwo');
+
+let isClickedtwo =  true;
+
+function showHidetwo(){
+    if(isClickedtwo){
+        clickPlus.style.display ='block';
+        isClickedtwo = false;
+    }else{
+        clickPlus.style.display ='none';
+        isClickedtwo = true;
+    }
+}
+// show or hide button 03
+
+const clickPlustwo = document.querySelector ('#linksandinfothree');
+
+let isClickedthree = true;
+
+function showHidethree(){
+    if(isClickedthree){
+        clickPlustwo.style.display='block';
+        isClickedthree = false;
+    }else{
+        clickPlustwo.style.display='none';
+        isClickedthree = true;
+    }
+}
+
+// show or hide button 04 
+
+const clickPlusthree = document.querySelector('#linksandinfofour');
+let isClickedfour = true;
+
+function showHidefour(){
+    if(isClickedfour){
+        clickPlusthree.style.display ='block';
+        isClickedfour = false;
+    }else{
+        clickPlusthree.style.display ='none';
+        isClickedfour = true;
+    }
+}
+
+// show or hide button 05
+
+const clickPlusFive = document.querySelector('#linksandinfofive');
+
+let isClickedFive = true;
+
+function showHidefive(){
+    if(isClickedFive){
+        clickPlusFive.style.display='block';
+        isClickedFive = false;
+    } else{
+        clickPlusFive.style.display='none';
+        isClickedFive = true;
+    }
+}
+
+// show or hide button 06
+
+
+const clickPlusSix = document.querySelector('#linksandinfosix');
+
+let isClickedSix = true;
+
+function showHidesix(){
+    if(isClickedSix){
+        clickPlusSix.style.display='block';
+        isClickedSix = false;
+    } else{
+        clickPlusSix.style.display='none';
+        isClickedSix = true;
+    }
+}
+
+
